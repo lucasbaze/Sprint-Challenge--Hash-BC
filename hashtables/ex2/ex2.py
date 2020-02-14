d0de262a980533d144ac029c63d1ffaaffff7df6
Lucas Bazemore <lucasabazemore@gmail.com>
2020-02-14 13:14:10 -0600

    completed ex2 hashtables

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -13,11 +13,22 @@ class Ticket:
 
 
 def reconstruct_trip(tickets, length):
-    hashtable = HashTable(length)
+    ht = HashTable(length)
     route = [None] * length
 
-    """
-    YOUR CODE HERE
-    """
+    # Put each ticket in the hash table by the source
+    for item in tickets: 
+        hash_table_insert(ht, item.source, item.destination)
 
-    pass
+    # get the first source of None
+    nextDestination = hash_table_retrieve(ht, 'NONE')
+    route[0] = nextDestination
+    hash_table_remove(ht, 'NONE')
+
+    # Iterate through the rest
+    for x in range(1, length - 1):
+        route[x] = hash_table_retrieve(ht, nextDestination)
+        nextDestination = hash_table_retrieve(ht, nextDestination)
+
+    return [x for x in route if x is not None]
+
